28e8c6d3e80f6e6a656254d391549e983afcc75b
Jack Jansen <Jack.Jansen@cwi.nl>
2018-04-30 01:41:41 +0200

    Implemented first attempt at coap handlers

--- src/iotsaCoapApi.cpp
+++ src/iotsaCoapApi.cpp
@@ -5,32 +5,89 @@
 
 class CoapEndpoint {
 public:
-  CoapEndpoint(IotsaApiProvider *_provider, bool _get, bool _put, bool _post)
+  CoapEndpoint(IotsaApiProvider *_provider, const char *_path, bool _get, bool _put, bool _post)
   : provider(_provider),
+    path(_path),
     get(_get),
     put(_put),
     post(_post),
-    next(NULL)
+    next(NULL),
+    coap(NULL)
   {}
   IotsaApiProvider *provider;
+  const char *path;
   bool get;
   bool put;
   bool post;
   CoapEndpoint *next;
+  Coap* coap;
+
   void callbackImpl(CoapPacket &pkt, IPAddress ip, int port);
 
-//  CoapCallback myCallback();
 };
 
 void CoapEndpoint::callbackImpl(CoapPacket &pkt, IPAddress ip, int port) {
-
+    bool ok = false;
+    String replyData;
+    // Handle requests, after chaing that type (get/put/post) is supported.
+    if (pkt.type == COAP_GET) {
+        IFDEBUG IotsaSerial.print("COAP-GET api ");
+        IFDEBUG IotsaSerial.println(path);
+        ok = get;
+        if (ok) {
+            DynamicJsonBuffer replyBuffer;
+            JsonObject& reply = replyBuffer.createObject();
+            ok = provider->getHandler(path, reply);
+            if (ok) {
+                reply.printTo(replyData);
+            }
+        }
+    }
+    if (pkt.type == COAP_PUT) {
+        IFDEBUG IotsaSerial.print("COAP-PUT api ");
+        IFDEBUG IotsaSerial.println(path);
+        ok = put && pkt.type == COAP_APPLICATION_JSON;
+        if (ok) {
+            char dataBuffer[pkt.payloadlen+1];
+            memcpy(dataBuffer, pkt.payload, pkt.payloadlen);
+            dataBuffer[pkt.payloadlen] = 0;
+            DynamicJsonBuffer requestBuffer;
+            JsonObject& request = requestBuffer.parseObject(dataBuffer);
+            DynamicJsonBuffer replyBuffer;
+            JsonObject& reply = replyBuffer.createObject();
+            ok = provider->putHandler(path, request, reply);
+            if (ok) {
+                reply.printTo(replyData);
+            }
+        }
+    }
+    if (pkt.type == COAP_POST) {
+        IFDEBUG IotsaSerial.print("COAP-POST api ");
+        IFDEBUG IotsaSerial.println(path);
+        ok = post && pkt.type == COAP_APPLICATION_JSON;
+        if (ok) {
+            char dataBuffer[pkt.payloadlen+1];
+            memcpy(dataBuffer, pkt.payload, pkt.payloadlen);
+            dataBuffer[pkt.payloadlen] = 0;
+            DynamicJsonBuffer requestBuffer;
+            JsonObject& request = requestBuffer.parseObject(dataBuffer);
+            DynamicJsonBuffer replyBuffer;
+            JsonObject& reply = replyBuffer.createObject();
+            ok = provider->postHandler(path, request, reply);
+            if (ok) {
+                reply.printTo(replyData);
+            }
+        }
+    }
+    // Send reply, either a JSON datastructure or an error.
+    if (ok) {
+        coap->sendResponse(ip, port, pkt.messageid, replyData.c_str(), replyData.length(), COAP_CONTENT, COAP_APPLICATION_JSON, NULL, 0);
+        IFDEBUG IotsaSerial.println("-> OK");
+    } else {
+        coap->sendResponse(ip, port, pkt.messageid, NULL, 0, COAP_BAD_REQUEST, COAP_NONE, NULL, 0);
+        IFDEBUG IotsaSerial.println("-> ERR");
+    }
 }
-
-#if 0
-CoapCallback CoapEndpoint::myCallback() {
-    return &std::bind(&CoapEndpoint::callbackImpl, this);
-}
-#endif
 
 class IotsaCoapServiceMod : public IotsaBaseMod {
 public:
@@ -72,8 +129,8 @@ IotsaCoapApiService::IotsaCoapApiService(IotsaApiProvider* _provider, IotsaAppli
 }
 
 void IotsaCoapApiService::setup(const char* path, bool get, bool put, bool post) {
+    CoapEndpoint *ep = new CoapEndpoint(provider, path, get, put, post);
     if (strncmp(path, "/api/", 5) == 0) path += 5;
-    CoapEndpoint *ep = new CoapEndpoint(provider, get, put, post);
     _coapMod->addEndpoint(ep, path);
 }
 
