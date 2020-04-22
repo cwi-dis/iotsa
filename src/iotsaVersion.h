1639f9e0f482932a594d71f76463d2960243fff7
Jack Jansen <Jack.Jansen@cwi.nl>
2020-04-23 01:23:59 +0200

    Version number was reported wrong. Fixed.

--- src/iotsaVersion.h
+++ src/iotsaVersion.h
@@ -1,3 +1,3 @@
-#define IOTSA_VERSION "2.1"
-#define IOTSA_COMMIT ""
-#define IOTSA_FULL_VERSION ("2.1")
+#define IOTSA_VERSION "2.4"
+#define IOTSA_COMMIT "064dde5"
+#define IOTSA_FULL_VERSION ("2.4""@""064dde5")
