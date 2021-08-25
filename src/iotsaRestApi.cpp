47df50e6fe682d756ef7947947f97e640dd4ee13
Jack Jansen <Jack.Jansen@cwi.nl>
2021-08-25 11:23:03 +0200

    JSON buffers for REST calls would overflow silently. Detected and allow fixing with jsonBufSize parameter

--- src/iotsaRestApi.cpp
+++ src/iotsaRestApi.cpp
@@ -12,9 +12,18 @@ void IotsaRestApiService::_getHandlerWrapper(const char *path) {
     if (auth && !auth->allows(path, IOTSA_API_GET)) return;
     IFDEBUG IotsaSerial.print("GET api ");
     IFDEBUG IotsaSerial.println(path);
-    DynamicJsonDocument replyDocument(2048);
+    int jsonBufSize = 2048;
+    if (server->hasArg("jsonBufSize")) {
+        jsonBufSize = server->arg("jsonBufSize").toInt();
+    }
+    DynamicJsonDocument replyDocument(jsonBufSize);
     JsonObject reply = replyDocument.to<JsonObject>();
     bool ok = provider->getHandler(path, reply);
+    if (replyDocument.overflowed()) {
+        server->send(413, "text/plain", "JSON document overflow, use jsonBufSize");
+        IFDEBUG IotsaSerial.println("-> ERR JSON document overflow");
+        return;
+    }
     if (ok) {
         String replyData;
         serializeJson(replyDocument, replyData);
@@ -22,7 +31,7 @@ void IotsaRestApiService::_getHandlerWrapper(const char *path) {
         IFDEBUG IotsaSerial.println("-> OK");
     } else {
         server->send(400, "text/plain", "\"bad request\"");
-        IFDEBUG IotsaSerial.println("-> ERR");
+        IFDEBUG IotsaSerial.println("-> ERR bad request");
     }
 }
 
@@ -30,12 +39,26 @@ void IotsaRestApiService::_putHandlerWrapper(const char *path) {
     if (auth && !auth->allows(path, IOTSA_API_PUT)) return;
     IFDEBUG IotsaSerial.print("PUT api ");
     IFDEBUG IotsaSerial.println(path);
-    DynamicJsonDocument replyDocument(2048);
+    int jsonBufSize = 2048;
+    if (server->hasArg("jsonBufSize")) {
+        jsonBufSize = server->arg("jsonBufSize").toInt();
+    }
+    DynamicJsonDocument replyDocument(jsonBufSize);
     JsonObject reply = replyDocument.to<JsonObject>();
-    DynamicJsonDocument requestDocument(2048);
+    DynamicJsonDocument requestDocument(jsonBufSize);
     deserializeJson(requestDocument, server->arg("plain"));
+    if (requestDocument.overflowed()) {
+        server->send(413, "text/plain", "JSON document overflow, use jsonBufSize");
+        IFDEBUG IotsaSerial.println("-> ERR JSON document overflow");
+        return;
+    }
     JsonObject request = requestDocument.as<JsonObject>();
     bool ok = provider->putHandler(path, request, reply);
+    if (replyDocument.overflowed()) {
+        server->send(413, "text/plain", "JSON document overflow, use jsonBufSize");
+        IFDEBUG IotsaSerial.println("-> ERR JSON document overflow");
+        return;
+    }
     if (ok) {
         String replyData;
         serializeJson(replyDocument, replyData);
@@ -43,7 +66,7 @@ void IotsaRestApiService::_putHandlerWrapper(const char *path) {
         IFDEBUG IotsaSerial.println("-> OK");
     } else {
         server->send(400, "text/plain", "\"bad request\"");
-        IFDEBUG IotsaSerial.println("-> ERR");
+        IFDEBUG IotsaSerial.println("-> ERR bad request");
     }
 }
 
@@ -51,12 +74,26 @@ void IotsaRestApiService::_postHandlerWrapper(const char *path) {
     if (auth && !auth->allows(path, IOTSA_API_POST)) return;
     IFDEBUG IotsaSerial.print("POST api ");
     IFDEBUG IotsaSerial.println(path);
-    DynamicJsonDocument replyDocument(2048);
+    int jsonBufSize = 2048;
+    if (server->hasArg("jsonBufSize")) {
+        jsonBufSize = server->arg("jsonBufSize").toInt();
+    }
+    DynamicJsonDocument replyDocument(jsonBufSize);
     JsonObject reply = replyDocument.to<JsonObject>();
-    DynamicJsonDocument requestDocument(2048);
+    DynamicJsonDocument requestDocument(jsonBufSize);
     deserializeJson(requestDocument, server->arg("plain"));
+    if (requestDocument.overflowed()) {
+        server->send(413, "text/plain", "JSON document overflow, use jsonBufSize");
+        IFDEBUG IotsaSerial.println("-> ERR JSON document overflow");
+        return;
+    }
     JsonObject request = requestDocument.as<JsonObject>();
     bool ok = provider->postHandler(path, request, reply);
+    if (replyDocument.overflowed()) {
+        server->send(413, "text/plain", "JSON document overflow, use jsonBufSize");
+        IFDEBUG IotsaSerial.println("-> ERR JSON document overflow");
+        return;
+    }
     if (ok) {
         String replyData;
         serializeJson(replyDocument, replyData);
