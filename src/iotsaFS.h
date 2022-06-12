a5809d7453084f5ef9815a812b1ba0e97b661735
Jack Jansen <Jack.Jansen@cwi.nl>
2022-06-12 13:03:41 +0200

    Cater for include file name differing in case between builtin littlefs and lorol littlefs

--- src/iotsaFS.h
+++ src/iotsaFS.h
@@ -7,16 +7,19 @@
 #include <FS.h>
 
 #ifdef IOTSA_WITH_LEGACY_SPIFFS
+// Use SPIFFS. On ESP32 it requires an extra include, on 8266 not.
 #ifdef ESP32
 #include <SPIFFS.h>
 #endif
 #define IOTSA_FS SPIFFS
 #define IOTSA_FS_NAME "SPIFFS"
 #else
-#ifdef ESP32
+#if defined(ESP32) && !defined(PLATFORMIO)
+// Use Lorol LittleFS when building for Arduino. It uses slightly different include file name.
 #include <LITTLEFS.h>
 #define IOTSA_FS LITTLEFS
 #else
+// Use normal (as of 2022) LittleFS on esp32 or esp8266.
 #include <LittleFS.h>
 #define IOTSA_FS LittleFS
 #endif
