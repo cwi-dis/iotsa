ba1342169464409cc5e7e52988d72bb09c2bb912
Jack Jansen <Jack.Jansen@cwi.nl>
2022-07-30 10:58:53 +0200

    Fixed the way the asyncio loop is created and reused. Things are starting to work.

--- extras/python/iotsa/ble.py
+++ extras/python/iotsa/ble.py
@@ -10,9 +10,11 @@ class BLE:
     def __init__(self):
         self._allDevices = None
         self._currentDevice = None
+        self._currentConnection = None
+        self.loop = asyncio.new_event_loop()
 
     def findDevices(self):
-        asyncio.run(self._asyncFindDevices())
+        self.loop.run_until_complete(self._asyncFindDevices())
         return self._allDevices
 
     async def _asyncFindDevices(self):
@@ -29,7 +31,7 @@ class BLE:
 
     def selectDevice(self, name_or_address):
         print('xxxjack before async select')
-        asyncio.run(self._asyncSelectDevice(name_or_address))
+        self.loop.run_until_complete(self._asyncSelectDevice(name_or_address))
         print('xxxjack after async select')
         return self._currentDevice != None
 
@@ -44,28 +46,35 @@ class BLE:
         if not dev:
             print(f'Device {name_or_address} not found')
             return
-        self._currentDevice = bleak.pythonic.client.BleakPythonicClient(dev)
+        self._currentDevice = dev
+        self._currentConnection = bleak.pythonic.client.BleakPythonicClient(self._currentDevice)
         print('xxxjack async select ended')
 
     def printStatus(self):
         print('xxxjack before async print status')
-        asyncio.run(self._asyncPrintStatus())
+        
+        self.loop.run_until_complete(self._asyncPrintStatus())
         print('xxxjack after async print status')
 
     async def _asyncPrintStatus(self):
-        services = await self._currentDevice.get_services()
-        print(f"services {services}")
+        async with self._currentConnection as client:
+            services = await client.get_services()
+        for handle, char in services.characteristics.items():
+            value = None # await client.read_gatt_char_typed(char)
+            print(f'{handle}: {char.uuid} ({char.description}): {value}')
 
     def set(self, name, value):
-        asyncio.run(self._asyncSet(name, value))
+        self.loop.run_until_complete(self._asyncSet(name, value))
 
     async def _asyncSet(self, name, value):
-        await self._currentDevice.write_gatt_char_typed(name, value, True)
+        async with self._currentConnection as client:
+            await client.write_gatt_char_typed(name, value, True)
 
     def get(self, name):
         self._get_rv = None
-        asyncio.run(self._asyncGet(name))
+        self.loop.run_until_complete(self._asyncGet(name))
         return self._get_rv
 
     async def _asyncGet(self, name):
-        self._get_rv = await self._currentDevice.read_gatt_char_typed(name)
+        async with self._currentConnection as client:
+            self._get_rv = await client.read_gatt_char_typed(name)
