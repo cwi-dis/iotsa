2b01a4d872ed2008221f92b0258bd4475ceebf27
Jack Jansen <Jack.Jansen@cwi.nl>
2022-08-01 10:39:05 +0200

    Ran black

--- extras/mkversionh.py
+++ extras/mkversionh.py
@@ -5,7 +5,8 @@ import os
 import re
 import subprocess
 
-DEFINE_PAT=re.compile(r'^#define\s+(?P<name>\w+)\s+(?P<value>.*)\s+$')
+DEFINE_PAT = re.compile(r"^#define\s+(?P<name>\w+)\s+(?P<value>.*)\s+$")
+
 
 class VersionFile:
     def __init__(self, includeFile):
@@ -15,49 +16,59 @@ class VersionFile:
         for line in open(self.includeFile):
             match = DEFINE_PAT.match(line)
             if match:
-                self.defines[match.group('name')] = match.group('value')
-                
+                self.defines[match.group("name")] = match.group("value")
+
     def define(self, name, value):
         if self.defines.get(name, None) == value:
             return
         self.defines[name] = value
         self.changed = True
-        
+
     def save(self):
-        fp = open(self.includeFile, 'w')
+        fp = open(self.includeFile, "w")
         for name, value in list(self.defines.items()):
-            fp.write('#define %s %s\n' % (name, value))
-            
+            fp.write("#define %s %s\n" % (name, value))
+
     def get(self, key, default=None):
         return self.defines.get(key, default)
-            
+
+
 def main():
     progName = os.path.join(os.getcwd(), sys.argv[0])
     baseDir = os.path.dirname(os.path.dirname(progName))
-    libraryConfig = os.path.join(baseDir, 'library.json')
-    version = os.path.join(baseDir, 'src', 'iotsaVersion.h')
+    libraryConfig = os.path.join(baseDir, "library.json")
+    version = os.path.join(baseDir, "src", "iotsaVersion.h")
     if not os.path.exists(libraryConfig) or not os.path.exists(version):
-        print('%s: Cannot find config files %s and %s. Must be run from iotsa source tree.'% (sys.argv[0], libraryConfig, version))
+        print(
+            "%s: Cannot find config files %s and %s. Must be run from iotsa source tree."
+            % (sys.argv[0], libraryConfig, version)
+        )
         sys.exit(1)
-        
+
     vf = VersionFile(version)
-    
+
     libraryData = json.load(open(libraryConfig))
-    if 'version' in libraryData:
-        vf.define('IOTSA_VERSION', '"'+libraryData['version']+'"')
-        
-    cmd = subprocess.Popen('git rev-parse --short HEAD', shell=True, cwd=baseDir, stdout=subprocess.PIPE, universal_newlines=True)
+    if "version" in libraryData:
+        vf.define("IOTSA_VERSION", '"' + libraryData["version"] + '"')
+
+    cmd = subprocess.Popen(
+        "git rev-parse --short HEAD",
+        shell=True,
+        cwd=baseDir,
+        stdout=subprocess.PIPE,
+        universal_newlines=True,
+    )
     commit = cmd.stdout.read().strip()
     if commit:
-        vf.define('IOTSA_COMMIT', '"'+commit+'"')
-    fullVersion = vf.get('IOTSA_VERSION', '"unknown""')
-    commit = vf.get('IOTSA_COMMIT')
+        vf.define("IOTSA_COMMIT", '"' + commit + '"')
+    fullVersion = vf.get("IOTSA_VERSION", '"unknown""')
+    commit = vf.get("IOTSA_COMMIT")
     if commit:
         fullVersion = "(" + fullVersion + '"@"' + commit + ")"
     vf.define("IOTSA_FULL_VERSION", fullVersion)
     if vf.changed:
         vf.save()
-        
-if __name__ == '__main__':
+
+
+if __name__ == "__main__":
     main()
-    
