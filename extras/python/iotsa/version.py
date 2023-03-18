9021f2e5d617ccff6cba4f2073b44c35c245a987
Jack Jansen <Jack.Jansen@cwi.nl>
2023-03-18 18:31:36 +0100

    Getting things ready to experiment with BLE REST server

--- extras/python/iotsa/version.py
+++ extras/python/iotsa/version.py
@@ -1,2 +1,2 @@
-__version__ = "v2.6-6-g62b1c7c"
+__version__ = "v2.6-9-gbe98e8a"
 __shortversion__ = "2.6"
--- src/iotsaVersion.h
+++ src/iotsaVersion.h
@@ -1,2 +1,2 @@
 #define IOTSA_VERSION "2.6"
-#define IOTSA_FULL_VERSION "v2.6-6-g62b1c7c"
+#define IOTSA_FULL_VERSION "v2.6-9-gbe98e8a"
