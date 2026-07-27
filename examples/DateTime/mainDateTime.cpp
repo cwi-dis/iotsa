5174b76b9a9818ccbf46c2d31ae5ac899f40ff78
Jack Jansen <Jack.Jansen@cwi.nl>
2026-07-27 22:37:23 +0200

    Remove WITH_BATTERY from DateTime, not part of its story
    Cloned from Skeleton (like Input's flags, see previous commit) but
    unrelated to what this example teaches - RTC/NTP time sync, not
    power management. Also updated the stale top comment, still describing
    generic Skeleton boilerplate rather than what this example actually
    does.

--- examples/DateTime/mainDateTime.cpp
+++ examples/DateTime/mainDateTime.cpp
@@ -1,21 +1,14 @@
 //
-// Boilerplate for configurable web server (probably RESTful) running on ESP8266.
-//
-// The server always includes the Wifi configuration module. You can enable
-// other modules with the preprocessor defines. With the default defines the server
-// will allow serving of web pages and other documents, and of uploading those.
+// Demonstrates keeping a real-time clock (DS1302) in sync using NTP.
 //
 
 #include <Arduino.h>
 #include "iotsa.h"
 #include "iotsaWifi.h"
 
-// CHANGE: Add application includes and declarations here
-
 #define WITH_RTC    // Enable Realtime Clock support
 #define WITH_NTP    // Use network time protocol to synchronize the clock.
 #define WITH_OTA    // Enable Over The Air updates from ArduinoIDE. Needs at least 1MB flash.
-#define WITH_BATTERY // Enable power-saving support
 
 IotsaApplication application("Iotsa DateTime Server");
 IotsaWifiMod wifiMod(application);
@@ -41,18 +34,9 @@ IotsaNtpMod ntpMod(application, authProvider);
 IotsaOtaMod otaMod(application, authProvider);
 #endif
 
-#ifdef WITH_BATTERY
-#define PIN_DISABLE_SLEEP 0 // Define for pin on which low signal disables sleep
-#include "iotsaBattery.h"
-IotsaBatteryMod batteryMod(application, authProvider);
-#endif
-
 void setup(void){
   application.setup();
   application.serverSetup();
-#ifdef PIN_DISABLE_SLEEP
-  batteryMod.setPinDisableSleep(PIN_DISABLE_SLEEP);
-#endif
 }
  
 void loop(void){
