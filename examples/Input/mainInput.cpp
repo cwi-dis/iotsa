0f3e27167d70e04911fbec0cd2e05fd058f1fb1a
Jack Jansen <Jack.Jansen@cwi.nl>
2026-07-27 22:24:28 +0200

    Trim Input example down to what it actually does
    Removed WITH_USER/WITH_NTP/WITH_FILES*/WITH_BATTERY - unrelated infra
    flags cloned from Skeleton, never trimmed. The example only ever
    printed button/encoder state to serial, so none of that was doing
    anything. Kept WITH_OTA (opt-out), renamed the app from "Iotsa Input
    Server" to "Iotsa Input Sample" since IotsaInputMod serves no web/REST
    endpoints at all.

--- examples/Input/mainInput.cpp
+++ examples/Input/mainInput.cpp
@@ -1,29 +1,23 @@
 //
-// Boilerplate for configurable web server (probably RESTful) running on ESP8266.
-//
-// The server always includes the Wifi configuration module. You can enable
-// other modules with the preprocessor defines. With the default defines the server
-// will allow serving of web pages and other documents, and of uploading those.
+// Demonstrates the Input abstraction (iotsaInput.h): a rotary encoder and a
+// pushbutton, both reporting their state changes to the serial console.
 //
 
 #include <Arduino.h>
 #include "iotsa.h"
 #include "iotsaWifi.h"
+#include "iotsaInput.h"
 
-// CHANGE: Add application includes and declarations here
-
-#undef WITH_USER   // Enable username/password authentication for changing configurations
-#undef WITH_NTP    // Use network time protocol to synchronize the clock.
 #define WITH_OTA    // Enable Over The Air updates from ArduinoIDE. Needs at least 1MB flash.
-#undef WITH_FILES  // Enable static files webserver
-#undef WITH_FILESUPLOAD  // Enable upload of static files for webserver
-#undef WITH_FILESBACKUP  // Enable backup of all files including config files and webserver files
-#undef WITH_BATTERY // Enable power-saving support
 
-IotsaApplication application("Iotsa Input Server");
+IotsaApplication application("Iotsa Input Sample");
 IotsaWifiMod wifiMod(application);
 
-#include "iotsaInput.h"
+#ifdef WITH_OTA
+#include "iotsaOta.h"
+IotsaOtaMod otaMod(application);
+#endif
+
 // When using an Alps EC12D rotary encoder with pushbutton here is the pinout:
 // When viewed from the top there are pins at northwest, north, northeast, southwest, southeast.
 // These pins are named (in Alps terminology) A, E, B, C, D.
@@ -42,53 +36,11 @@ Input* inputs[] = {
 
 IotsaInputMod inputMod(application, inputs, sizeof(inputs)/sizeof(inputs[0]));
 
-#ifdef WITH_USER
-#include "iotsaUser.h"
-IotsaUserMod userMod(application);
-#define authProvider &userMod
-#else
-#define authProvider NULL
-#endif
-
-#ifdef WITH_NTP
-#include "iotsaNtp.h"
-IotsaNtpMod ntpMod(application, authProvider);
-#endif
-
-#ifdef WITH_OTA
-#include "iotsaOta.h"
-IotsaOtaMod otaMod(application, authProvider);
-#endif
-
-#ifdef WITH_FILES
-#include "iotsaFiles.h"
-IotsaFilesMod filesMod(application);
-#endif
-
-#ifdef WITH_FILESUPLOAD
-#include "iotsaFilesUpload.h"
-IotsaFilesUploadMod filesUploadMod(application, authProvider);
-#endif
-
-#ifdef WITH_FILESBACKUP
-#include "iotsaFilesBackup.h"
-IotsaFilesBackupMod filesBackupMod(application, authProvider);
-#endif
-
-#ifdef WITH_BATTERY
-#define PIN_DISABLE_SLEEP 0 // Define for pin on which low signal disables sleep
-#include "iotsaBattery.h"
-IotsaBatteryMod batteryMod(application, authProvider);
-#endif
-
 void setup(void){
   application.setup();
   application.serverSetup();
-#ifdef PIN_DISABLE_SLEEP
-  batteryMod.setPinDisableSleep(PIN_DISABLE_SLEEP);
-#endif
 }
- 
+
 int oldButtonValue = -1;
 int oldEncoderValue = -1;
 
